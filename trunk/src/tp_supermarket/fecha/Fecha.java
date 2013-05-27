cc62eca9c0f7f549112d7d68dba37e4696c17262
manuolmos88@gmail.com <manuolmos88@gmail.com@abd6f89a-e12d-b15e-afac-684da346a216>
2013-05-27 01:10:22 +0000

    cambie la enie por ni para no generar conflictos

--- trunk/src/tp_supermarket/fecha/Fecha.java
+++ trunk/src/tp_supermarket/fecha/Fecha.java
@@ -11,9 +11,9 @@ public class Fecha {
 		this.fecha = Calendar.getInstance();
 	}
 
-	public Fecha(int dia, int mes, int año) {
+	public Fecha(int dia, int mes, int anio) {
 		this.fecha = Calendar.getInstance();
-		this.fecha.set(año, mes - 1, dia, 00, 00, 00);
+		this.fecha.set(anio, mes - 1, dia, 00, 00, 00);
 	}
 
 	public Calendar getFecha() {
