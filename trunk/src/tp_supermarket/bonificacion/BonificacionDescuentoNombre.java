6eda2824b60c4a69edac33fe1a8af7519a6cceda
MatiasJA@gmail.com <MatiasJA@gmail.com@abd6f89a-e12d-b15e-afac-684da346a216>
2013-05-27 03:57:18 +0000

    +Promociones por nombre de producto

---
+++ trunk/src/tp_supermarket/bonificacion/BonificacionDescuentoNombre.java
@@ -0,0 +1,54 @@
+package tp_supermarket.bonificacion;
+
+import java.util.ArrayList;
+
+import tp_supermarket.producto.Producto;
+import tp_supermarket.restriccion.Restriccion;
+
+public class BonificacionDescuentoNombre extends Bonificacion {
+	
+	String nombre;
+	int  cant;
+	float porcentaje;
+	
+	
+	public BonificacionDescuentoNombre(String n, int c, float p ) {
+		// TODO Auto-generated constructor stub
+		this.nombre=n;
+		this.cant=c;
+		this.porcentaje=p;
+	}
+	
+	public BonificacionDescuentoNombre(String n, float p ) {
+		// TODO Auto-generated constructor stub
+		this.nombre=n;
+		this.cant=1;
+		this.porcentaje=p;
+	}
+
+
+	@Override
+	public ArrayList<Producto> bonificar(ArrayList<Producto> misproducts, ArrayList<Restriccion> excepciones) {
+		ArrayList<Producto> descuentos= new ArrayList<Producto>();
+		float costo =buscarPrecio(nombre, misproducts);
+		float descuento = ((costo * porcentaje)/100)*-1;
+		Producto nuevoDescuento = new Producto(0,"Descuento de " + porcentaje+ " % por " + nombre,descuento);
+		descuentos.add(nuevoDescuento);
+		return descuentos;
+	}
+
+
+	private float buscarPrecio(String marca2, ArrayList<Producto> misproducts) {
+		float result=0 ;
+		for (int i=0;i<misproducts.size();i++){			
+			if(misproducts.get(i).getNombre()== nombre){
+				result=misproducts.get(i).getCosto();
+				i=misproducts.size();
+			}
+			
+		}
+		return result;
+	}
+	
+
+}
