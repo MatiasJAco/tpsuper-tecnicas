d1b1256502046177ae2cc77b7abdbd3a14c0cc50
manuolmos88@gmail.com <manuolmos88@gmail.com@abd6f89a-e12d-b15e-afac-684da346a216>
2013-05-30 17:22:01 +0000

    Agregue un par de Test Cases mas a la caja

--- trunk/src/src/tp_supermarket/tests/TestCaja.java
+++ trunk/src/src/tp_supermarket/tests/TestCaja.java
@@ -2,24 +2,42 @@ package tp_supermarket.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
 
 import tp_supermarket.caja.Caja;
+import tp_supermarket.caja.ExceptionAbrirCajaConCajaAbierta;
+import tp_supermarket.caja.ExceptionTerminarCompraConCajaCerrada;
+import tp_supermarket.caja.ExceptionTerminarCompraConCompraNoIniciada;
+import tp_supermarket.caja.MedioDePago;
 import tp_supermarket.caja.cerrarCajaConCompraEnCurso;
 import tp_supermarket.caja.exceptions.ExceptionCerrarCajaConCajaCerrada;
 import tp_supermarket.caja.exceptions.ExceptionCerrarCajaConCompraEnCurso;
 import tp_supermarket.caja.exceptions.ExceptionIniciarCompraConCajaCerrada;
 import tp_supermarket.caja.exceptions.ExceptionIniciarCompraConCompraEnCurso;
+import tp_supermarket.producto.Producto;
 
 public class TestCaja {
+
+	private Caja unaCaja;
+
+	@Before
+	public void setUp() throws Exception {
+		unaCaja = new Caja(1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
 	public void testCerrarCajaConCajaCerrada()
 			throws cerrarCajaConCompraEnCurso {
-		Caja cajaTest = new Caja(1);
 		boolean estabaCerrada = false;
 		boolean estaCerrada = true;
 		try {
-			cajaTest.cerrarCaja();
+			unaCaja.cerrarCaja();
 		} catch (ExceptionCerrarCajaConCajaCerrada e) {
 		} catch (ExceptionCerrarCajaConCompraEnCurso e) {
 			estabaCerrada = true;
@@ -31,13 +49,12 @@ public class TestCaja {
 	@Test
 	public void testCerrarCajaConCompraEnCurso()
 			throws cerrarCajaConCompraEnCurso {
-		Caja unaCaja = new Caja(1);
 		unaCaja.setCompraEnCurso(true);
 		try {
 			unaCaja.cerrarCaja();
 		} catch (ExceptionCerrarCajaConCajaCerrada e) {
 		} catch (ExceptionCerrarCajaConCompraEnCurso e) {
-			assertEquals(unaCaja.isEstadoCaja(), false);
+			assertEquals(false, unaCaja.isEstadoCaja());
 		}
 
 	}
@@ -45,16 +62,56 @@ public class TestCaja {
 	@Test
 	public void testIniciarCompraConCompraCajaCerrada()
 			throws cerrarCajaConCompraEnCurso {
-		Caja unaCaja = new Caja(1);
 		unaCaja.setEstadoCaja(true);
 		try {
 			unaCaja.iniciarCompra();
 		} catch (ExceptionIniciarCompraConCajaCerrada e) {
 		} catch (ExceptionIniciarCompraConCompraEnCurso e) {
-			assertEquals(unaCaja.isEstadoCaja(), true);
+			assertEquals(true, unaCaja.isEstadoCaja());
 			assertNull(unaCaja.getCompraActual());
-			assertEquals(unaCaja.isCompraEnCurso(), false);
+			assertEquals(false, unaCaja.isCompraEnCurso());
 		}
 
+	}
+
+	@Test
+	public void testTerminarCompraNoTieneCompraActual()
+			throws ExceptionIniciarCompraConCajaCerrada,
+			ExceptionIniciarCompraConCompraEnCurso,
+			ExceptionAbrirCajaConCajaAbierta, ExceptionTerminarCompraConCajaCerrada, ExceptionTerminarCompraConCompraNoIniciada {
+		System.out.println("Caja cerrada: " + unaCaja.isEstadoCaja());
+		unaCaja.abrirCaja();
+		unaCaja.iniciarCompra();
+		Producto unProducto = new Producto(1, "Coca cola", 10);
+		unaCaja.agregarProducto(unProducto); // un producto
+		unaCaja.terminarCompraActual(new MedioDePago());
+		assertEquals(null, unaCaja.getCompraActual());
+		assertEquals(false, unaCaja.isCompraEnCurso());
+	}
+
+	@Test
+	public void testTerminarCompraNoAgregaCompra()
+			throws ExceptionIniciarCompraConCajaCerrada,
+			ExceptionIniciarCompraConCompraEnCurso,
+			ExceptionAbrirCajaConCajaAbierta, ExceptionTerminarCompraConCajaCerrada, ExceptionTerminarCompraConCompraNoIniciada{
+		unaCaja.abrirCaja();
+		unaCaja.iniciarCompra();
+		int cantCompras = unaCaja.getCompras().size();
+		Producto unProducto = new Producto(1, "Coca cola", 10);
+		unaCaja.agregarProducto(unProducto);
+		unaCaja.terminarCompraActual(new MedioDePago());
+		// verifico que se haya agregado la nueva compra, es decir tiene que
+		// tener una compras mas
+		assertEquals(unaCaja.getCompras().size(), cantCompras + 1);
+	}
+
+	@Test
+	public void testAbrirYCerrarCaja() throws ExceptionAbrirCajaConCajaAbierta, ExceptionCerrarCajaConCompraEnCurso, ExceptionCerrarCajaConCajaCerrada {
+			boolean cajaCerrada = false;
+			unaCaja.abrirCaja();
+			assertEquals(cajaCerrada, unaCaja.isEstadoCaja());
+			unaCaja.cerrarCaja();
+			cajaCerrada = true;
+			assertEquals(cajaCerrada, unaCaja.isEstadoCaja());
 	}
 }
