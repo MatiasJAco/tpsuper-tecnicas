88c34f952fd49af20fceca6e827dcdbbcfd4400d
manuolmos88@gmail.com <manuolmos88@gmail.com@abd6f89a-e12d-b15e-afac-684da346a216>
2013-06-02 07:44:18 +0000

    Listo ya estan funcionando las 3 primers pruebas

--- src/src/tp_supermarket/tests/mainTest.java
+++ src/src/tp_supermarket/tests/mainTest.java
@@ -78,9 +78,6 @@ public class mainTest {
 		 */
 		ArrayList<MedioDePago> mDePagos = new ArrayList<MedioDePago>();
 		mDePagos.add(new MedioDePago("XYZ",""));
-		/*
-		 * Bonificacion por medio de pago
-		 */
 		
 		Promocion promo1 = new Promocion(restricciones, excepciones,
 				bonificaciones, mDePagos);
@@ -117,28 +114,27 @@ public class mainTest {
 		assertEquals(12.6f,cajaprincipal.getCompras().get(0).getTotalCD(),0.00001);
 	}
 	
-//	@Test
+	@Test
 	public void test2() throws ExceptionTerminarCompraConCajaCerrada, ExceptionTerminarCompraConCompraNoIniciada, ExceptionAbrirCajaConCajaAbierta {
 
 		/*
 		 * Productos
 		 */
-		Producto art1 = new Producto(1, "CocaCola", 1, "Bebidas",
-				"CocaCola", "");
-		Producto art2 = new Producto(2, "Maceta", 10, "Jardineria", "Maceta",
+		Producto art1 = new Producto(1, "Maceta", 10, "Jardineria", "Maceta",
 				"");
-		Producto art3 = new Producto(3, "Cepillos de Dientes", 3, "Salud",
+		Producto art2 = new Producto(2, "Cepillos de Dientes", 3, "Salud",
 				"Cepi", "");
-		Producto art4 = new Producto(4, "Vino X", 100, "Vinoteca",
+		Producto art3 = new Producto(3, "Vino X", 100, "Vinoteca",
 				"X", "");
-		Producto art5 = new Producto(5, "Chandon", 75, "Vinoteca",
+		Producto art4 = new Producto(4, "Chandon", 75, "Vinoteca",
 				"Chandon", "");
 		ArrayList<Producto> misproducts = new ArrayList<Producto>();
 		misproducts.add(art1);
 		misproducts.add(art2);
 		misproducts.add(art3);
+		misproducts.add(art3);
 		misproducts.add(art4);
-		misproducts.add(art5);
+		misproducts.add(art4);
 
 		/*
 		 * Restricciones
@@ -164,48 +160,31 @@ public class mainTest {
 		/*
 		 * Nueva promo
 		 */
-
-		Promocion promo1 = new Promocion(restricciones, excepciones,
-				bonificaciones);
-
-		
-		//-------------------------------------------------
-		/*
-		 * Restricciones
-		 */
-		ArrayList<Restriccion> restricciones2 = new ArrayList<Restriccion>();
-
-		/*
-		 * Bonificaciones
-		 */
-		BonificacionDescuentoMedioDePago bDescuento = new BonificacionDescuentoMedioDePago(
-				10);
-		ArrayList<Bonificacion> bonificaciones2 = new ArrayList<Bonificacion>();
-
-		bonificaciones.add(bDescuento);
-		/*
-		 * Excepciones
-		 */
-		ArrayList<Restriccion> excepciones2 = new ArrayList<Restriccion>();	
-
 		ArrayList<MedioDePago> mDePagos = new ArrayList<MedioDePago>();
 		mDePagos.add(new MedioDePago("Tarjeta de Debito",""));
+
+		Promocion promo1 = new Promocion(restricciones, excepciones,
+				bonificaciones,mDePagos);
 		
-		Promocion promo2 = new Promocion(restricciones2, excepciones2,bonificaciones2
-				,mDePagos);
+		ArrayList<Integer> diasPromo = new ArrayList<Integer>();		
+		diasPromo.add(Calendar.SUNDAY);
+		PeriodoValidez pValidez = new PeriodoValidezDiasSemana(diasPromo);
+		promo1.setPeriodoValidezPromocion(pValidez);
+		
+
+
+		
 		/*
 		 * Seteo de la fecha de vigencia de la promocion
 		 */
-		ArrayList<Integer> diasPromo = new ArrayList<Integer>();		
-		diasPromo.add(Calendar.MONDAY);
-		PeriodoValidez pValidez = new PeriodoValidezDiasSemana(diasPromo);
-		promo1.setPeriodoValidezPromocion(pValidez);
-		promo2.setPeriodoValidezPromocion(pValidez);
+		ArrayList<Integer> diasPromo1 = new ArrayList<Integer>();		
+		diasPromo1.add(Calendar.SUNDAY);
+		PeriodoValidez pValidez1 = new PeriodoValidezDiasSemana(diasPromo1);
+		promo1.setPeriodoValidezPromocion(pValidez1);
 
 		ArrayList<Promocion> misPromociones = new ArrayList<Promocion>();
 		;
 		misPromociones.add(promo1);
-		misPromociones.add(promo2);
 		// Caja cajaprincipal = new Caja(1234);
 
 		Caja cajaprincipal = new Caja(1234, misPromociones);
@@ -213,21 +192,21 @@ public class mainTest {
 		try {
 			cajaprincipal.iniciarCompra();
 
-			cajaprincipal.agregarProducto(art4);
-			cajaprincipal.agregarProducto(art4);
-			cajaprincipal.agregarProducto(art5);
-			cajaprincipal.agregarProducto(art5);
-			cajaprincipal.agregarProducto(art3);
+			cajaprincipal.agregarProducto(art1);
 			cajaprincipal.agregarProducto(art2);
+			cajaprincipal.agregarProducto(art3);
+			cajaprincipal.agregarProducto(art3);
+			cajaprincipal.agregarProducto(art4);
+			cajaprincipal.agregarProducto(art4);
 
-			MedioDePago med = new MedioDePago("Tarjeta de Debito", "");
+			MedioDePago med = new MedioDePago("Tarjeta de Debito", "",10,pValidez1);
 			cajaprincipal.terminarCompraActual(med);
 
 		} catch (ExceptionIniciarCompraConCajaCerrada e) {
 		} catch (ExceptionIniciarCompraConCompraEnCurso e) {
 		}
 
-		assertEquals(12.4f,cajaprincipal.getCompras().get(0).getTotalCD(),0.00001);
+		assertEquals(259.2f,cajaprincipal.getCompras().get(0).getTotalCD(),0.00001);
 	}
 	
 	@Test
